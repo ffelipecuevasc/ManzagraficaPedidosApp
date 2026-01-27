7b02f92e0dd9ae5e0b7711b2f0d9450a35c3e53a
Felipe Cuevas <ffelipecuevasc@gmail.com>
2026-01-26 23:14:50 -0300

    Parche Seguridad: Cambio de URL módulo de administración por defecto en Django.

--- PedidosApp/urls.py
+++ PedidosApp/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("acceso-manzagrafica-admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pedidos.urls')),
 ]
